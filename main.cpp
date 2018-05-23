98d745838d1c66b7f9ca0b21bd3ba71f664fc974
ZHUJingyuan43119 <39566382+ZHUJingyuan43119@users.noreply.github.com>
2018-05-23 19:34:04 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include"TD.h"
+#include<iostream>
+#include<string>
+#include<conio.h>
+#include<Windows.h>
+#include <stdlib.h> 
+#include <time.h>  
+#include <fstream> 
+#include <ctime> 
+
+using namespace std;
+
+int main(){
+	CControle c;
+	c.start();
+	system("pause");
+	return 0;
+}
